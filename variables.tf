de67be74d7426a7f82215587ff34df863b1a1797
Han Ken <hanken16@gmail.com>
2021-10-18 14:32:26 +0800

    update variables

--- variables.tf
+++ variables.tf
@@ -16,9 +16,9 @@ resource "random_string" "suffix" {
 
 locals {
   cluster_name          = "eks-${random_string.suffix.result}"
-  availability_zone     = ["ap-southeast-1a"] #, "ap-southeast-1b", "ap-southeast-1c"]
+  availability_zone     = ["ap-southeast-1a", "ap-southeast-1b"] #, "ap-southeast-1c"]
   work_node1_size       = 1
   work_node2_size       = 0
-  private_subnets       = ["10.0.1.0/24"] #, "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets        = ["10.0.4.0/24"] #, "10.0.5.0/24", "10.0.6.0/24"]
+  private_subnets       = ["10.0.1.0/24", "10.0.2.0/24"] #, "10.0.3.0/24"]
+  public_subnets        = ["10.0.4.0/24", "10.0.5.0/24"] #, "10.0.6.0/24"]
 }
